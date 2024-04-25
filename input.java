5972261c4719be01959be3b3b6a7dddf67345b85
yash sehgal <155468916+realyashsehgal@users.noreply.github.com>
2024-04-25 19:29:55 +0530

    taking the input in java

---
+++ input.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+public class input {
+    public static void main(String[] args) {
+        Scanner boom = new Scanner(System.in);
+        String booooom = boom.nextLine();
+        System.out.println(booooom);
+        boom.close();
+    }
+}
